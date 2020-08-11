7c6af0adbc7fe201ad802d93e212263a5ac68044
mas rosyid <rosyidgrobogan@gmail.com>
2020-08-11 17:13:16 +0700

    detail page markup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const category = "SNEAKER";
+  const price = 768000;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div class="App">
+      <div class="Photo">
+        <img src="sneaker.jpg" />
+      </div>
+      <div class="Description">
+        <p class="Cate">{category}</p>
+        <h1 class="Title">Pinky Swagg</h1>
+        <p class="Price">IDR {price}</p>
+        <p class="Info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora,
+          nihil laborum blanditiis veritatis, qui nostrum dolorem rerum
+          exercitationem tenetur dolore consectetur suscipit vel ratione,
+          eligendi magnam ad ab enim eos.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
